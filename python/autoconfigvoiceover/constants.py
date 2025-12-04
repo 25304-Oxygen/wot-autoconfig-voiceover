4a6e5c24b827acf1ad97c6d4617cedd450732416
25304-Oxygen <2240873057@qq.com>
2025-12-04 11:52:47 +0800

    Update version number

--- python/autoconfigvoiceover/constants.py
+++ python/autoconfigvoiceover/constants.py
@@ -6,7 +6,7 @@ import os.path
 import ResMgr
 MY_MODS_ID = 'autoConfigVoiceOver'
 MY_MODS_LINKAGE = 'auto_config_voiceover'
-MY_MODS_VERSION = '0.0.6b'
+MY_MODS_VERSION = '0.0.7'
 MY_MODS_CONFIG_VERSION = 3
 VOICE_SELECTED_EVENT = 'vo_selected'                                            # 选中语音并应用后，将播放的一个 Wwise 事件
 TANKMEN_XML_ROOT_PATH = 'scripts/item_defs/tankmen/'                            # ResMgr 中的虚拟路径
